76ef473c6b2e9428a02118d2fe4e87ec0a1e4cd1
justinmilushev <justin@milushev.com>
2019-09-15 14:30:45 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/GamerOp.java
@@ -0,0 +1,135 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+/**
+ * Created by Justin Milushev on 9/7/2018.
+ */
+public class GamerOp extends OpMode {
+    //initialize objects
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx leftFrontDrive, leftRearDrive, rightFrontDrive, rightRearDrive;
+    private boolean precision, direction;
+    private boolean canTogglePrecision, canToggleDirection, strafeMode;
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity; //might delete
+
+    @Override
+    public void init() {
+        //after driver hits init
+        setUpMotors();
+
+        precision = false;
+        direction = false;
+        strafeMode = false;
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    //what runs in between robot being initialized and before it plays
+    public void init_loop() {
+
+    }
+
+    //once driver hits play
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+
+    @Override
+    public void loop() {
+        telemetry.update();
+        driveBot();
+    }
+
+    public void driveBot() {
+        // Precision mode toggled by pressing A
+        if (gamepad1.right_stick_button && canTogglePrecision) {
+            precision = !precision;
+            canTogglePrecision = false;
+        }
+        else if (!gamepad1.right_stick_button)
+            canTogglePrecision = true;
+
+        if (gamepad1.left_stick_button && canToggleDirection) {
+            direction = !direction;
+            canTogglePrecision = false;
+        }
+        else if (!gamepad1.left_stick_button)
+            canToggleDirection = true;
+
+        double x = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double stickAngle = Math.atan2(direction ? -gamepad1.left_stick_y : gamepad1.left_stick_y, direction ? gamepad1.left_stick_x : -gamepad1.left_stick_x); // desired robot angle from the angle of stick
+
+        if (gamepad1.right_bumper)
+            strafeMode = !strafeMode;
+
+        double powerAngle = 0;
+
+        if (!strafeMode)
+            powerAngle = stickAngle - (Math.PI / 4); // conversion for correct power values
+        else
+            powerAngle = stickAngle - (3 * Math.PI / 4); // conversion for correct power values
+
+        double rightX = gamepad1.right_stick_x;
+
+        final double leftFrontPower = Range.clip(x * Math.cos(powerAngle) - rightX, -1.0, 1.0);
+        final double leftRearPower = Range.clip(x * Math.sin(powerAngle) - rightX, -1.0, 1.0);
+        final double rightFrontPower = Range.clip(x * Math.sin(powerAngle) + rightX, -1.0, 1.0);
+        final double rightRearPower = Range.clip(x * Math.cos(powerAngle) + rightX, -1.0, 1.0);
+
+        //(Code Below) If (?) precision than 0.1 else (:) 1
+        leftFrontDrive.setPower(leftFrontPower * (precision ? 0.2 : 0.7));
+        leftRearDrive.setPower(leftRearPower * (precision ? 0.2 : 0.7));
+        rightFrontDrive.setPower(rightFrontPower * (precision ? 0.2 : 0.7));
+        rightRearDrive.setPower(rightRearPower * (precision ? 0.2 : 0.7));
+
+        // Send DS wheel power values.
+        telemetry.addData("Front Motors", "Left Front (%.2f), Right Front (%.2f)", leftFrontPower, rightFrontPower);
+        telemetry.addData("Rear Motors", "Left Rear (%.2f), Right Rear (%.2f)", leftRearPower, rightRearPower);
+    }
+
+    public void setUpMotors() {
+        leftFrontDrive = (DcMotorEx) hardwareMap.dcMotor.get("leftFront");
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRearDrive = (DcMotorEx) hardwareMap.dcMotor.get("leftBack");
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive = (DcMotorEx) hardwareMap.dcMotor.get("rightFront");
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRearDrive = (DcMotorEx) hardwareMap.dcMotor.get("rightBack");
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Left Motor is being reversed as its direction is reverse of what is needed to drive forward
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
