c4ed5aa1505ddaed023ccb7c35de95fc679f05e8
justinmilushev <justin@milushev.com>
2020-01-18 16:57:02 -0600

    Revision: Refined Motion Profiling
    MNotion profiling now allows for the input of a set initial and final power for desired motion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MotionProfiling/MotionProfiler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MotionProfiling/MotionProfiler.java
@@ -7,19 +7,20 @@ public class MotionProfiler {
         accelProp = accelProportion;
     }
 
-    public double getProfilePower(double propTravelled, double maxPower) {
-        double multiplier = maxPower / (2 * Math.pow(accelProp / 2, 2));
+    public double getProfilePower(double propTravelled, double maxPower, double initPower, double finPower) {
+        double accelMult = (maxPower - initPower) / (2 * Math.pow(accelProp / 2, 2));
+        double decelMult = (maxPower - finPower) / (2 * Math.pow(accelProp / 2, 2));
         if (propTravelled < accelProp / 2) {
-            return multiplier * Math.pow(propTravelled, 2);
+            return accelMult * Math.pow(propTravelled, 2) + initPower;
         } else if (propTravelled < accelProp) {
-            return 2 * multiplier * Math.pow(accelProp / 2, 2) - multiplier * Math.pow(propTravelled - accelProp, 2);
+            return maxPower - accelMult * Math.pow(propTravelled - accelProp, 2);
         } else if (propTravelled < 1 - accelProp) {
-            return 2 * multiplier * Math.pow(accelProp / 2, 2);
+            return maxPower;
         } else if (propTravelled < 1 - accelProp / 2) {
-            return 2 * multiplier * Math.pow(accelProp / 2, 2) - multiplier * Math.pow(propTravelled - (1 - accelProp), 2);
+            return maxPower - decelMult * Math.pow(propTravelled - (1 - accelProp), 2);
         } else if (propTravelled < 1) {
-            return multiplier * Math.pow(propTravelled - 1,  2);
+            return decelMult * Math.pow(propTravelled - 1, 2) + finPower;
         }
-        return 0;
+        return finPower;
     }
 }
